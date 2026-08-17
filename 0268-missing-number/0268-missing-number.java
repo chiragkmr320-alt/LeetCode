47c0f0e83b9b4adcfafca2e3667e9005416d40fc
chiragkmr320-alt <chiragkmr320@gmail.com>
2026-08-17 09:54:33 +0530

    Time: 0 ms (100%), Space: 47.5 MB (33.46%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i]!=i){
-                return i;
-            }
-        }
-        return nums.length;
+       int xor = nums.length;
+       for(int i=0;i<nums.length;i++){
+        xor ^= i;
+        xor ^=nums[i];
+       }
+       return xor;
     }
 }
