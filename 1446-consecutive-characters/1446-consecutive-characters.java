878691118d90b6331ad9c55c2a1b77dad014b184
chiragkmr320-alt <chiragkmr320@gmail.com>
2026-08-19 11:47:15 +0530

    Time: 1 ms (100%), Space: 43.6 MB (26.28%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxPower(String s) {
+        int count =1;
+        int maxcount =1;
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                count++;
+                if(count > maxcount){
+                    maxcount = count;
+                }
+            }else{
+                count = 1;
+            }
+        }
+        return maxcount;
+    }
+}
